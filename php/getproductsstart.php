0aad716c92dc7d5a3a505a5eb485d4a96dd29aaf
huerkamp01 <huerkamp01@DE-SG-PC-21597.technotrans.lan>
2017-06-12 14:05:10 +0200

    Dynamische Thumbnails
    -Neue .php Datei für dynamische Thumbnails
    -Nur die ersten 6 Spalten werden gelesen
    -Anpassung von Starseite.php für die Thumbnails

---
+++ php/getproductsstart.php
@@ -0,0 +1,44 @@
+<?php 
+
+include ("connectdb.php");
+
+$conn->set_charset('utf8');
+
+$sql = "SELECT * FROM produkte";
+
+$db_erg = mysqli_query($conn, $sql) or die (mysqli_error($conn));
+if ( ! $db_erg )
+{
+	die('Ungültige Abfrage...');
+}
+
+
+$i = 0;
+while ($zeile = mysqli_fetch_array( $db_erg))
+{
+	$i++;
+	if($i <=6)
+	{
+    print(" <div class=\"col-sm-4 col-lg-4 col-md-4\"> ");
+	print(" <div class=\"thumbnail\"> ");
+	print(" <img src=\"http://placehold.it/320x150\" alt=\"\"> ");
+	print(" <div class=\"caption\"> ");
+	print(" <h4 class=\"pull-right\"> $zeile[Preis] </h4> ");
+	print(" <h4><a href=\"#\"> $zeile[Produktname] </a> ");
+	print(" </h4> ");
+	print(" <p> $zeile[Beschreibung] </p> ");
+	print(" </div> ");
+	print(" </div> ");
+	print(" </div> ");
+	}
+	else{
+		break;
+	}
+	//print($i);
+}
+
+
+mysqli_free_result( $db_erg );
+
+
+?>

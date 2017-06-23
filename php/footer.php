19d6b139bb9debbc347d17085172fa6b0f959d97
Marcel <Marcel@Marcel-Rechner>
2017-06-23 21:19:19 +0200

    - Footer PHP erstellt

---
+++ php/footer.php
@@ -0,0 +1,24 @@
+<footer style="
+background-color: #c0c0c0;">
+
+	<div class="container" style="padding: 25px 0;">
+		<div class="row">
+			<div class="col-md-offset-1 col-md-5 border">
+				<h3>Über uns</h3>					
+			</div>
+			
+			<div class="col-md-5 border">
+				<h3>Kontaktinformationen</h3>
+				Berufskolleg Beckum des Kreises Warendorf <br>
+				Hansaring 11  <br>
+				59269 Beckum <br> <br>
+				
+				Telefon: +49 2521 912-0 <br>
+				Telefax: +49 2521 912-131 <br>
+				E-Mail: verwaltung@bkbeckum.de <br>
+				Internet: <a href="www.berufskolleg-beckum.de">www.berufskolleg-beckum.de</a> <br>
+				
+			</div>
+		</div>
+	</div>
+</footer>

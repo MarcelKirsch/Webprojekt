f9c219aa5d7441f20cda86a3392cf1a44569298a
jonhu_000 <jonhu_000@RuleZ.Speedport_W_921V_1_43_000>
2017-06-24 13:53:44 +0200

    Code Geordnet

--- php/getcategoriesart.php
+++ php/getcategoriesart.php
@@ -5,21 +5,16 @@ $conn->set_charset ( 'utf8' );
 
 $sql = "SELECT * FROM kategorie";
 
-
-
 $db_erg = mysqli_query ( $conn, $sql ) or die ( mysqli_error ( $conn ) );
 if (! $db_erg) {
-	die ( 'Ungültige Abfrage...' );
+	die ( 'UngÃ¼ltige Abfrage...' );
 }
 
-
-
 while ( $zeile = mysqli_fetch_array ( $db_erg ) ) {
-	// print (" <option value="$zeile['KATID']"> $zeile[Katname] </option>  ") ;
+	// print (" <option value="$zeile['KATID']"> $zeile[Katname] </option> ") ;
 	echo '
-		<option value="'.$zeile['KatID'] .'"> ' .$zeile['Katname'] .' </option>
+		<option value="' . $zeile ['KatID'] . '"> ' . $zeile ['Katname'] . ' </option>
 	';
-	
 }
 
 mysqli_free_result ( $db_erg );

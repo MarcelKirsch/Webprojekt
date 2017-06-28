14a52b40cd534aa1cd4b93eb9978abd00708a6cf
jonhu_000 <jonhu_000@RuleZ.Speedport_W_921V_1_43_000>
2017-06-28 23:20:02 +0200

    Sonstiges

--- webcontent/piseite3.php
+++ webcontent/piseite3.php
@@ -76,53 +76,7 @@
 		</div>
 		
 		
-		<!-- Anderer Artikel des Verkäufers -->
-		<div class="row container-body">
-		<h2>Anderer Artikel des Verkäufers</h2>
-			
-			
-			<!-- Bilder Carousel -->
-			<div class="col-md-8 carousel-artikel">
-				<div id="carousel-02" class="carousel slide"
-					data-ride="carousel">
-					<ol class="carousel-indicators">
-						<li data-target="#carousel-02" data-slide-to="0"
-							class="active"></li>
-						<li data-target="#carousel-02" data-slide-to="1"></li>
-						<li data-target="#carousel-02" data-slide-to="2"></li>
-					</ol>
-					<div class="carousel-inner">
-						<div class="item active">
-							<img class="slide-image" src="http://placehold.it/800x300" alt="">
-						</div>
-						<div class="item">
-							<img class="slide-image" src="http://placehold.it/800x300" alt="">
-						</div>
-						<div class="item">
-							<img class="slide-image" src="http://placehold.it/800x300" alt="">
-						</div>
-					</div>
-						<a class="left carousel-control" href="#carousel-02"
-							data-slide="prev"> <span
-							class="glyphicon glyphicon-chevron-left"></span>
-						</a> <a class="right carousel-control"
-							href="#carousel-02" data-slide="next"> <span
-							class="glyphicon glyphicon-chevron-right"></span>
-						</a>
-				</div>
-			</div>
-			
-			
-			<!-- Text zu den Bildern -->
-			<div class="col-md-5">
-				<div class="artikel-beschreibung">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut 
-				labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et 
-				ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
-				Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut 
-				labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et 
-				ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum</div>
-			</div>
-		</div>
+		
 		
 		
 		

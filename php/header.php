7a05790346d5dfc7955e7041b6aede77bf7a3df2
Marcel <Marcel@Marcel-Rechner>
2017-06-28 18:56:35 +0200

    - Quellcode Änderungen

--- php/header.php
+++ php/header.php
@@ -13,11 +13,9 @@
 					src="../img/logo_trans.png" class="logo">
 				</a>
 				
-				<!-- Suchleiste -->
-				<!-- role="search" -->
 				<form class="navbar-form navbar-left"  method="post"  enctype="multipart/form-data">
 					<div class="form-group input-group-lg searchbar">
-						<input type="text" name="search" style="width: 90%" class="form-control"
+						<input type="text" name="search" class="form-control"
 							placeholder="Suchen">
 						<button type="submit"  name="suchego" class="btn btn-default btn-lg">Los</button>
 					</div>
@@ -26,32 +24,29 @@
 				<?php if(!isset($_SESSION['loggedin'])){
 					echo '
 							<a href="../artikel-verkaufen.php">
-					<button type="button" class="btn btn-default button-anzeige" disabled>Artikel
-						einstellen</button>
+								<button type="button" class="btn btn-default button-anzeige" disabled>Artikel
+								einstellen</button>
 				</a>
 						 ';
 				}
 				else{
 					echo '
 							<a href="../webcontent/artikel-verkaufen.php">
-					<button type="button" class="btn btn-default button-anzeige">Artikel
-						einstellen</button>
-				</a>
+								<button type="button" class="btn btn-default button-anzeige">Artikel
+								einstellen</button>
+							</a>
 						 ';
 				}?>
 
 				<ul class="nav navbar-nav navbar-right" id="profilbutton-class">
-					
-			             
-			             
-			                    <?php
+								 <?php
 									if (! isset ( $_SESSION ['loggedin'] )) {
 										echo '
 											<li class="dropdown"><a href="#"
 											class="profilbutton dropdown-toggle" data-toggle="dropdown"></a>                                      
-											<ul class="dropdown-menu dropdown-lr animated slideInRight" role="menu" style="width: 250px">
-				                            <div class="text-center border" style="border: 1px solid #e3e3e3; margin: -7px 0 12px 0; background-color: #e3e3e3"><h4><b>Anmelden</b></h4></div>
-						                        <div class="col-md-12">
+												<ul class="dropdown-menu dropdown-lr animated slideInRight" role="menu" style="width: 250px">
+				                            	<div class="text-center border" style="border: 1px solid #e3e3e3; margin: -7px 0 12px 0; background-color: #e3e3e3"><h4><b>Anmelden</b></h4></div>
+						                        	<div class="col-md-12">
 						                          
 						                            <form id="ajax-login-form" action="#" method="post" role="form" autocomplete="off">
 						                                <div class="form-group">
@@ -80,33 +75,33 @@
 									} else {
 									echo '
 								<li class="dropdown"><a href="#"
-					class="profilbutton dropdown-toggle" data-toggle="dropdown" style=background-image:url("../img/Flash.jpg"); background-position: center; background-size: cover;></a>
+									class="profilbutton dropdown-toggle" data-toggle="dropdown" style=background-image:url("../img/Flash.jpg"); background-position: center; background-size: cover;></a>
 									<ul class="dropdown-menu dropdown-lr animated slideInRight" role="menu" style="width: 250px">
 										<div class="text-center border" id="dropdown-css"><h4><b>Kontrollzentrum</b></h4></div>
 											<div class="col-md-12">
 												<div class="form-group">
-												<div class="row ">
-												<div class="col-md-3" style="padding: 0; margin: 0;">
-												<div class="profilbild" style=background-image:url("../img/Flash.jpg")></div>
-												</div>
-												<div class="col-md-9">
-												<h5>'.$_SESSION['prename'].', '.$_SESSION['afternamelol'].'</h5>
-												<a href="../webcontent/eigenerbenutzer.php"><h5>Mein Profil</h5></a>
-												</div>
-												</div>
-												<hr>
+													<div class="row ">
+														<div class="col-md-3" style="padding: 0; margin: 0;">
+															<div class="profilbild" style=background-image:url("../img/Flash.jpg")></div>
+															</div>
+															<div class="col-md-9">
+																<h5>'.$_SESSION['prename'].', '.$_SESSION['afternamelol'].'</h5>
+																<a href="../webcontent/eigenerbenutzer.php"><h5>Mein Profil</h5></a>
+															</div>
+													</div>
+													<hr>
 												</div>
 												<div class="form-group">
-												<div class="row ">
-												<div class="col-md-3">
-												<span class="glyphicon glyphicon-list-alt" style="font-size: 4.2rem;" aria-hidden="true"></span>
-												</div>
-												<div class="col-md-9 pull-left">
-												</h2><a href="..\webcontent\benutzerartikel.php"><h5>Meine Artikel</h5></a>
-												</div>
-												</div>
+													<div class="row ">
+														<div class="col-md-3">
+															<span class="glyphicon glyphicon-list-alt" style="font-size: 4.2rem;" aria-hidden="true"></span>
+														</div>
+														<div class="col-md-9 pull-left">
+															</h2><a href="..\webcontent\benutzerartikel.php"><h5>Meine Artikel</h5></a>
+														</div>
+													</div>
 												</div> <hr>
-												</div>
+											</div>
 												<div class="text-center">
 												<a href="../php/logout.php">												
 													<div class="btn btn-danger" style="min-width: 140px; margin: 0 0 8px 0">
